d61b2eeaf96186df1819e2ac043aff0eba85dcd3
Zero <john.royer@gmail.com>
2022-02-18 21:40:17 +0800

    Add Crawler main class prototype

---
+++ src/Crawler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Johnroyer\Crawler;
+
+class Crawler
+{
+    protected $startUrl = '';
+
+    /**
+     */
+    public function __construct()
+    {
+        // initialize options
+    }
+
+    public function __destruct()
+    {
+        // TODO: save crawler status
+    }
+}
