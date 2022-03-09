6304ff13f0fa998d9c7753a6f91239d8b4ed46d9
Zero <john.royer@gmail.com>
2022-03-09 12:36:21 +0800

    Add unit test for PHP array queue

--- tests/src/Queue/ArrayQueueTest.php
+++ tests/src/Queue/ArrayQueueTest.php
@@ -8,17 +8,54 @@ class ArrayQueueTest extends \PHPUnit\Framework\TestCase
 {
     protected $queue;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->queue = new ArrayQueue();
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
         $this->queue = null;
 
         parent::tearDown();
     }
 
+    public function testQueueSize()
+    {
+        $this->queue->push(1);
+        $this->queue->push(2);
+        $this->queue->push(3);
+
+        $this->assertEquals(
+            3,
+            $this->queue->getLength()
+        );
+
+        return $this->queue;
+    }
+
+    /**
+     * @depends testQueueSize
+     */
+    public function testPopOneItem($queue)
+    {
+        $this->assertEquals(
+            1,
+            $queue->pop()
+        );
+
+        return $queue;
+    }
+
+    /**
+     * @depends testPopOneItem
+     */
+    public function testPopSecondItem($queue)
+    {
+        $this->assertEquals(
+            2,
+            $queue->pop(),
+        );
+    }
 }
