939516bc0606c3b406e90ded87e69c2c0199e488
Zero <john.royer@gmail.com>
2022-03-13 15:17:17 +0800

    Class for finding links from HTML

--- src/LinkFetcher.php
+++ src/LinkFetcher.php
@@ -2,28 +2,33 @@
 
 namespace Zeroplex\Crawler;
 
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Symfony\Component\DomCrawler\Crawler;
 
 class LinkFetcher
 {
     protected $links = [];
+    protected $url;
     protected $response;
     protected $crawler;
 
-    public function __construct(Response $response)
+    public function __construct(String $url, Response $res)
     {
-        $this->response;
+        $this->url = $url;
+        $this->response = $res;
     }
 
     public function getLinks()
     {
         $this->crawler = new Crawler(
-            $this->response
+            $this->response->getBody()->getContents(),
+            $this->url
         );
-        $links = $this->crawler->links();
 
-        dd($links);
+        $links = $this->crawler->filter('a')->links();
+
+        var_dump($links);exit;
         return $links;
     }
 }
