c5ebe63782ae5bc8b06d98cbf051f6f176a58a19
Zero <john.royer@gmail.com>
2022-04-04 09:30:22 +0800

    Add domain mutator into Profile

--- src/Profile/Profile.php
+++ src/Profile/Profile.php
@@ -7,6 +7,13 @@ use Psr\Http\Message\ResponseInterface;
 
 abstract class Profile
 {
+    protected string $domain;
+
+    public function getDomain(): string
+    {
+        return $this->domain;
+    }
+
     abstract function isFatchable(RequestInterface $request): bool;
 
     abstract function handle(ResponseInterface $response): void;
