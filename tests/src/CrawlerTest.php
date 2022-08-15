631d6cf9f7c936de2fe76d35e7746073d7d97243
Zero <john.royer@gmail.com>
2022-08-15 16:41:17 +0800

    Add test case prototype

--- tests/src/CrawlerTest.php
+++ tests/src/CrawlerTest.php
@@ -2,7 +2,21 @@
 
 namespace Tests;
 
+use Zeroplex\Crawler\Crawler;
+
 class CrawlerTest extends TestCase
 {
+    private $cawler = null;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->cawler = new Crawler();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->cawler = null;
+    }
 }
