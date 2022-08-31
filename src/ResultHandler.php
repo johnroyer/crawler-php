53aeef2777cb3e92ec28251d7537dded43ac29ff
Zero <john.royer@gmail.com>
2022-08-31 13:25:06 +0800

    Add handler setter

--- src/ResultHandler.php
+++ src/ResultHandler.php
@@ -2,10 +2,11 @@
 
 namespace Zeroplex\Crawler;
 
+use Zeroplex\Crawler\Handler\AbstractHandler;
+
 class ResultHandler
 {
     protected $handlers = [];
-    protected $domains = [];
 
     public function __construct()
     {
@@ -14,6 +15,19 @@ class ResultHandler
     public function __destruct()
     {
         $this->handlers = null;
-        $this->domains = null;
+    }
+
+    public function addHandler(AbstractHandler $handler): ResultHandler
+    {
+        $domain = filter_var($handler->getDomain(), FILTER_VALIDATE_DOMAIN);
+        if (false === $domain) {
+            throw new \Exception('Handler does not have a valid domain');
+        }
+        if (array_key_exists($domain, $this->handlers)) {
+            throw new \Exception('Duplicated handler');
+        }
+        $this->handlers[$domain] = $domain;
+
+        return $this;
     }
 }
