b9bb356090884ac2238b90b169a83ae3d470575f
Zero <john.royer@gmail.com>
2022-11-04 13:21:13 +0800

    Add comment for domain handler

--- src/Handler/AbstractHandler.php
+++ src/Handler/AbstractHandler.php
@@ -16,7 +16,21 @@ abstract class AbstractHandler
      */
     abstract public function getDomain(): string;
 
+    /**
+     * Tells if the URL should be crawled or not
+     *
+     * @param RequestInterface $request HTTP request
+     * @return bool true if URL should be crawled, false if not
+     */
     abstract function shouldFetch(RequestInterface $request): bool;
 
+    /**
+     * HTTP response handler
+     *
+     * find, save, or whatever you want to do with the HTTP response
+     *
+     * @param ResponseInterface $response
+     * @return void
+     */
     abstract function handler(ResponseInterface $response): void;
 }
