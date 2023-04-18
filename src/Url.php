975a1a2c70c8afaf7aa6370cb55028361bf35920
Zero <john.royer@gmail.com>
2023-04-18 16:07:49 +0800

    Implement URL util

--- src/Url.php
+++ src/Url.php
@@ -2,15 +2,23 @@
 
 namespace Zeroplex\Crawler;
 
+use URL\Normalizer;
+
 class Url
 {
     public static function normalize(string $url): string
     {
-        //
+        return (new Normalizer($url, true, true))
+            ->normalize();
     }
 
     public static function trimFragment(string $url): string
     {
-        //
+        // remove '#' in tail
+        $position = strpos($url, '#');
+        if (false !== $position && 0 <= $position) {
+            $url = substr($url, 0, $position);
+        }
+        return $url;
     }
 }
