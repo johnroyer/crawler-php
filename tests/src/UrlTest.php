93fff327b8cd272610a45f0c4cbd5d927861fb57
Zero <john.royer@gmail.com>
2023-05-02 15:59:25 +0800

    Add test case for URL normalizer

--- tests/src/UrlTest.php
+++ tests/src/UrlTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use Zeroplex\Crawler\Url;
+
 class UrlTest extends TestCase
 {
     public function setUp(): void
@@ -12,5 +14,28 @@ class UrlTest extends TestCase
     public function tearDown(): void
     {
         parent::tearDown();
+    }
+
+    public function urlNormalizeProvider()
+    {
+        return [
+            ['http://test.com/', 'http://test.com:80'],
+            ['http://test.com/path', 'http://test.com/path'],
+            ['http://test.com/path/', 'http://test.com/path/'],
+            ['http://test.com/?a=1&b=2', 'http://test.com/?a=1&b=2'],
+            ['http://test.com/?a=1&b=2', 'http://test.com/?b=2&a=1'],
+            ['http://test.com/path/?a=1&b=2', 'http://test.com/path/?b=2&a=1'],
+        ];
+    }
+
+    /**
+     * @dataProvider urlNormalizeProvider
+     */
+    public function testUrlNormalize($expected, $input)
+    {
+        $this->assertEquals(
+            $expected,
+            Url::normalize($input),
+        );
     }
 }
