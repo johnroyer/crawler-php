e9dd79f7c51d2856760e5db88b3c2e256edc4583
Zero <john.royer@gmail.com>
2023-05-02 15:49:50 +0800

    Add URL test prototype

---
+++ tests/src/UrlTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+class UrlTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
