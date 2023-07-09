22f26b99a95b28a1e001bcd2bc03d9d6c0157213
Zero <john.royer@gmail.com>
2023-07-09 10:36:15 +0800

    add class prototype

---
+++ src/LinkFinder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zeroplex\Crawler;
+
+use GuzzleHttp\Psr7\Response;
+
+class LinkFinder
+{
+    public static function fromRequest(Response $response, string $curentUrl): array
+    {
+        //
+    }
+
+    public static function fromHtml(string $html, string $currentUrl): array
+    {
+        //
+    }
+}
