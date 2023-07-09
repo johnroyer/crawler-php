21d885b0fab239101db97f82a8e6f20f0391506a
Zero <john.royer@gmail.com>
2023-07-09 11:22:30 +0800

    get HTML from request

--- src/LinkFinder.php
+++ src/LinkFinder.php
@@ -8,7 +8,16 @@ class LinkFinder
 {
     public static function fromRequest(Response $response, string $curentUrl): array
     {
-        //
+        if (null === $response) {
+            throw new \Exception('Response is empty');
+        }
+
+        $html = $response->getBody()->getContents();
+
+        if (empty($html)) {
+            return [];
+        }
+        return self::fromHtml($html, $curentUrl);
     }
 
     public static function fromHtml(string $html, string $currentUrl): array
